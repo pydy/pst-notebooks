21e9edebff963a4ad0a00514ce44bc3b3e76c1b4
Peter Stahlecker <peter.stahlecker@gmail.com>
2026-05-12 16:46:09 +0200

    used the latest capabilities of pydy

--- gallery/plot_explorer_anomaly.py
+++ gallery/plot_explorer_anomaly.py
@@ -6,7 +6,6 @@ Explorer Anomaly
 Objectives
 ----------
 
-- Show how calculate reaction forces with System.evaluate_ode()
 - Show how to use some specific ode_solver
 - Show how to use ``evaluate_ode`` for the calculation of the contributing
   forces (reaction forces) of the antennae to the explorer.
@@ -43,12 +42,17 @@ Notes
 
 - The dimensions and the masses of the explorer and the antennas are, as well
   as the constants :math:`k_\textrm{{torque}}` and
-  :math:`\mu_\textrm{{torque}}` are courtesy **Dr. David Levinson** via
-  **Dr. Carlos Roithmayr**.
+  :math:`\mu_\textrm{{torque}}` are courtesy *Dr. David Levinson* via
+  *Dr. Carlos Roithmayr*.
 - The equations of motion are derived using Kane's method.
 - As there is dampening, the total energy of the system decreases.
 - As there are no external forces or torques, the angular momentum of the
   system must be constant.
+- The animation is difficult: At first the motion is very fast, then it
+  slows down substantially. Due to space limitation just a few parts are
+  shown.
+- While pydy version 0.9.4 is the latest release, the developmental version
+  of pydy must be used.
 
 **States**
 
@@ -112,20 +116,25 @@ O = me.Point('O')
 O.set_vel(N, 0)
 t = me.dynamicsymbols._t
 
-# where the antennae attach to the explorer
+# %%
+# Points where the antennae attach to the explorer.
 P0, P1, P2, P3 = sm.symbols('P0 P1 P2 P3', cls=me.Point)
 
-# Mass centers of the explorer and antennae
+# %%
+# Mass centers of the explorer and antennae.
 Aoe, Aoa0, Aoa1, Aoa2, Aoa3 = sm.symbols('Aoe Aoa0 Aoa1 Aoa2 Aoa3',
                                          cls=me.Point)
-
-# Coordinates of the mass center of the explorer
+# %%
+# Coordinates of the mass center of the explorer.
 x, y, z, ux, uy, uz = me.dynamicsymbols('x, y, z, ux, uy, uz')
-# Coordinates of body fixed frame of the explorer
+
+# %%
+# Coordinates of body fixed frame of the explorer.
 qex, qey, qez = me.dynamicsymbols('qex, qey, qez')
 uex, uey, uez = me.dynamicsymbols('uex, uey, uez')
 
-# Coordinates of the body fixed frames of the antennae
+#  %%
+# Coordinates of the body fixed frames of the antennae.
 q0x, q0z, u0x, u0z = me.dynamicsymbols('q0x, q0z, u0x, u0z')
 q1x, q1z, u1x, u1z = me.dynamicsymbols('q1x, q1z, u1x, u1z')
 q2y, q2z, u2y, u2z = me.dynamicsymbols('q2y, q2z, u2y, u2z')
@@ -140,51 +149,60 @@ aux2x, aux2y, aux2z, f2x, f2y, f2z = me.dynamicsymbols(
 aux3x, aux3y, aux3z, f3x, f3y, f3z = me.dynamicsymbols(
     'aux3x, aux3y, aux3z, f3x, f3y, f3z')
 
-# Place hlodes for the u_i.diff(t) in the reaction forces
+# %%
+# Place holders for the u_i.diff(t) in the reaction forces.
 rhs_list = [sm.symbols('rhs' + str(i)) for i in range(14)]
 
 m_e, m_a, rei, reo, shift, La, Le, dist = sm.symbols(
     'm_e m_a rei reo shift La Le dist')
 k_torque, mu_torque = sm.symbols('k_torque mu_torque')
 
+# %%
 # rot, rot1 for the kinematical differential equations.
 rot, rot1 = [], []
-# Explorer body frame
+
+# %%
+# Explorer body frame.
 Ae.orient_body_fixed(N, (qex, qey, qez), 'XYZ')
 rot.append(Ae.ang_vel_in(N))
 Ae.set_ang_vel(N, uex*Ae.x + uey*Ae.y + uez*Ae.z)
 rot1.append(Ae.ang_vel_in(N))
 
-# Antenna 0 body frame
+# %%
+# Antenna 0 body frame.
 Aa0.orient_body_fixed(Ae, (q0x, 0, q0z), 'XYZ')
 rot.append(Aa0.ang_vel_in(N))
 Aa0.set_ang_vel(Ae, u0x*Aa0.x + u0z*Aa0.z)
 rot1.append(Aa0.ang_vel_in(N))
 
-# Antenna 1 body frame
+# %%
+# Antenna 1 body frame.
 Aa1.orient_body_fixed(Ae, (q1x, 0, q1z), 'XYZ')
 rot.append(Aa1.ang_vel_in(N))
 Aa1.set_ang_vel(Ae, u1x*Aa1.x + u1z*Aa1.z)
 rot1.append(Aa1.ang_vel_in(N))
 
-# Antenna 2 body frame
+# %%
+# Antenna 2 body frame.
 Aa2.orient_body_fixed(Ae, (0, q2y, q2z), 'XYZ')
 rot.append(Aa2.ang_vel_in(N))
 Aa2.set_ang_vel(Ae, u2y*Aa2.y + u2z*Aa2.z)
 rot1.append(Aa2.ang_vel_in(N))
 
-# Antenna 3 body frame
+# %%
+# Antenna 3 body frame.
 Aa3.orient_body_fixed(Ae, (0, q3y, q3z), 'XYZ')
 rot.append(Aa3.ang_vel_in(N))
 Aa3.set_ang_vel(Ae, u3y*Aa3.y + u3z*Aa3.z)
 rot1.append(Aa3.ang_vel_in(N))
 
-# Set the points
-# Center of gravity of explorer
+# %%
+# Center of gravity of explorer.
 Aoe.set_pos(O, x*N.x + y*N.y + z*N.z)
 Aoe.set_vel(N, ux*N.x + uy*N.y + uz*N.z)
 
-# Points where the antennae attach to the explorer
+# %%
+# Points where the antennae attach to the explorer.
 P0.set_pos(Aoe, dist*Ae.y + shift*Ae.z)
 vP0 = P0.v2pt_theory(Aoe, N, Ae)
 P0.set_vel(N, vP0 + aux0x*Ae.x + aux0y*Ae.y + aux0z*Ae.z)
@@ -198,7 +216,8 @@ P3.set_pos(Aoe, -dist*Ae.x + shift*Ae.z)
 vP3 = P3.v2pt_theory(Aoe, N, Ae)
 P3.set_vel(N, vP3 + aux3x*Ae.x + aux3y*Ae.y + aux3z*Ae.z)
 
-# Set the mass centers of the antennae
+# %%
+# Set the mass centers of the antennae.
 Aoa0.set_pos(P0, La/2 * Aa0.y)
 Aoa0.v2pt_theory(P0, N, Aa0)
 Aoa1.set_pos(P1, -La/2 * Aa1.y)
@@ -206,9 +225,10 @@ Aoa1.v2pt_theory(P1, N, Aa1)
 Aoa2.set_pos(P2, La/2 * Aa2.x)
 Aoa2.v2pt_theory(P2, N, Aa2)
 Aoa3.set_pos(P3, -La/2 * Aa3.x)
-Aoa3.v2pt_theory(P3, N, Aa3)
+_ = Aoa3.v2pt_theory(P3, N, Aa3)
 
-# Set the torques on the antennae
+# %%
+# Set the torques on the antennae.
 torque0x = -(k_torque * q0x + mu_torque * u0x) * Aa0.x
 torque0z = - (k_torque * q0z + mu_torque * u0z) * Aa0.z
 
@@ -236,14 +256,18 @@ torques = [
     (P3, f3x*Ae.x + f3y*Ae.y + f3z*Ae.z),
 ]
 
-# Bodies and their inertias
-# Explorer is a hollow tube
+# %%
+# Bodies and their inertias.
+# Explorer is a hollow tube.
+
 iXXe = m_e/12 * (3*(rei**2 + reo**2) + Le**2)
 iZZe = m_e/2 * (rei**2 + reo**2)
 iYYe = iXXe
 Inertia_e = me.inertia(Ae, iXXe, iYYe, iZZe)
 explorer1 = me.RigidBody('explorer1', Aoe, Ae, m_e, (Inertia_e, Aoe))
-# antennea are rods
+
+# %%
+# antennea are rods.
 iRR = 1/12*m_a*La**2
 inertia_0 = me.inertia(Aa0, iRR, 0, iRR)
 link0 = me.RigidBody('link0', Aoa0, Aa0, m_a, (inertia_0, Aoa0))
@@ -289,7 +313,7 @@ kane = me.KanesMethod(
 
 fr, frstar = kane.kanes_equations(bodies, torques)
 eingepraegt = kane.auxiliary_eqs.subs({i.diff(t): rhs_list[j]
-                                        for j, i in enumerate(u_ind)})
+                                       for j, i in enumerate(u_ind)})
 
 
 # %%
@@ -317,11 +341,13 @@ def ode_solver(f, x0, ts, args=(), **kwargs):
     return solve_ivp(lambda t, x: f(x, t, *args), ts[[0, -1]], x0,
                      t_eval=ts, **kwargs).y.T
 
+
 # %%
 # Initialize System.
 
 sys = System(kane, ode_solver=ode_solver)
 
+# %%
 # Define the constants of the system.
 
 sys.constants = {
@@ -338,7 +364,7 @@ sys.constants = {
 }
 
 # %%
-#Set the initial_conditions.
+# Set the initial_conditions.
 
 
 sys.initial_conditions = {
@@ -375,9 +401,9 @@ sys.initial_conditions = {
     }
 
 # %%
-#In the force vector are the reaction forces and the virtual speeds.
-#As they do no work, they are set to zero. This is not really necessary as
-#System() will do this automatically, but it is done here for clarity.
+# In the force vector are the reaction forces and the virtual speeds.
+# As they do no work, they are set to zero. This is not really necessary as
+# System() will do this automatically, but it is done here for clarity.
 
 spec_sym = sys.specifieds_symbols
 sys.specifieds = {'symbols': spec_sym, 'values': np.zeros(12)}
@@ -400,21 +426,31 @@ eingepraegt_lam = sm.lambdify(F_r + qL + pL + rhs_list,
 # %%
 # Numerical Integration
 # ---------------------
+#
+# Ensure that for speed the cython generator is used and the linear system
+# solver is numpy. This is to get an efficient r.h.s. for the numerical
+# integration.
 
-sys.generate_ode_function(generator='cython', linear_sys_solver='numpy')
+_ = sys.generate_ode_function(generator='cython', linear_sys_solver='numpy')
 
-sys.times = np.linspace(0., 100.0, 250)
+# %%
+# Set the integration interval and the point of time where the result
+# is returned.
 
-resultat = sys.integrate(method='DOP853', atol=1.e-6, rtol=1.e-6)
+sys.times = np.linspace(0., 100.0, 2000)
+
+# %%
+# Set the kwargs of solve_ivp used in the ode_solver.
+resultat = sys.integrate(method='DOP853', atol=1.e-9, rtol=1.e-9)
 
 print('resultat shape', resultat.shape)
 
 # %%
-# Plot some generalized coordinates
+# Plot some generalized coordinates.
 
 bezeichnung = [str(i) for i in q_ind + u_ind]
 
-fig, ax = plt.subplots(4, 1, figsize=(8, 8), layout='constrained',
+fig, ax = plt.subplots(4, 1, figsize=(8, 9), layout='constrained',
                        sharex=True)
 for i in (17, 18, 19):
     begin = 0
@@ -424,22 +460,30 @@ for i in (17, 18, 19):
     ax[0].axhline(-1.35, color='black', lw=0.5, ls='--')
     ax[0].axhline(0.0, color='red', lw=0.5, ls='--')
     ax[0].set_title('Angular Velocity of the Explorer')
+    ax[0].set_ylabel('Angular Velocity [rad/s]')
 _ = ax[0].legend()
 
 for i in (0, 1, 2):
     ax[1].plot(sys.times[begin: resultat.shape[0]], resultat[begin:, i],
                label=bezeichnung[i])
-    ax[1].set_title('Various generalized coordinates as selected')
+    ax[1].set_title('Position of the center of gravity of the explorer')
+    ax[1].set_ylabel('Position [m]')
 _ = ax[1].legend()
 
 for i in (6, 7, 8, 9, 10, 11, 12, 13):
-    ax[2].plot(sys.times[begin: resultat.shape[0]], resultat[begin:, i],
+    ax[2].plot(sys.times[begin: resultat.shape[0]],
+               np.rad2deg(resultat[begin:, i]),
                label=bezeichnung[i])
+ax[2].set_title('Orientation of the Antennae, zero is the position normal '
+                'to the surface of the explorer')
+ax[2].set_ylabel('Orientation [deg]')
 _ = ax[2].legend()
 
 for i in (20, 21, 22, 23):
     ax[3].plot(sys.times[begin: resultat.shape[0]], resultat[begin:, i],
                label=bezeichnung[i])
+ax[3].set_title('Angular Velocity of the Antennae')
+ax[3].set_ylabel('Angular Velocity [rad/s]')
 ax[-1].set_xlabel('Time [s]')
 _ = ax[3].legend()
 
@@ -513,79 +557,77 @@ for i in range(4):
                     'in the Explorer Frame Ae')
     ax[i].legend()
 _ = ax[-1].set_xlabel('Time [s]')
-ax[0].plot(sys.times[begin:], summe_np[begin:], 'r--', label='Sum of all Forces')
+ax[0].plot(sys.times[begin:], summe_np[begin:], 'r--',
+           label='Sum of all Forces')
 _ = ax[0].legend()
 
 # %%
 # Animation using PyDy Visualization
 # ----------------------------------
-#
+
 # In this sphinx environment the animation does not work. The program was left
 # here to show the simplicity of the animation with PyDy Visualization compared
 # to Matplotlib.
-# A screen shot of the animation may be found here:
-# https://github.com/pydy/pst-notebooks the name is explorer1_anomaly.mow
 
-# %%
-from pydy.viz.shapes import Cylinder, Sphere
-from pydy.viz.scene import Scene
-from pydy.viz.visualization_frame import VisualizationFrame
-
+# from pydy.viz.shapes import Cylinder, Sphere
+# from pydy.viz.scene import Scene
+# from pydy.viz.visualization_frame import VisualizationFrame
+#
 # Define the right frames so the cylinders point in the Y direction
-Be, Ba0, Ba1, Ba2, Ba3 = sm.symbols('Be B0 B1 B2 B3', cls=me.ReferenceFrame)
+# Be, Ba0, Ba1, Ba2, Ba3 = sm.symbols('Be B0 B1 B2 B3', cls=me.ReferenceFrame)
 # Point to see easily how explorer is turning.
-P_red = me.Point('P_red')
-Be.orient_body_fixed(Ae, (-np.pi/2, 0, 0), 'XYZ')
-Ba1.orient_body_fixed(Aa1, (0, 0, np.pi), 'XYZ')
-Ba2.orient_body_fixed(Aa2, (0, 0, np.pi/2), 'XYZ')
-Ba3.orient_body_fixed(Aa3, (0, 0, -np.pi/2), 'XYZ')
+# P_red = me.Point('P_red')
+# Be.orient_body_fixed(Ae, (-np.pi/2, 0, 0), 'XYZ')
+# Ba1.orient_body_fixed(Aa1, (0, 0, np.pi), 'XYZ')
+# Ba2.orient_body_fixed(Aa2, (0, 0, np.pi/2), 'XYZ')
+# Ba3.orient_body_fixed(Aa3, (0, 0, -np.pi/2), 'XYZ')
 
-P_red.set_pos(Aoe, Le/4 * Be.y + reo * Be.z)
+# P_red.set_pos(Aoe, Le/4 * Be.y + reo * Be.z)
 
-farben = ['grey', 'blue', 'green', 'red', 'yellow']
-viz_frames = []
-mass_centers = [Aoe, Aoa0, Aoa1, Aoa2, Aoa3]
-frames = [Ae, Aa0, Ba1, Ba2, Ba3]
+# farben = ['grey', 'blue', 'green', 'red', 'yellow']
+# viz_frames = []
+# mass_centers = [Aoe, Aoa0, Aoa1, Aoa2, Aoa3]
+# frames = [Ae, Aa0, Ba1, Ba2, Ba3]
 
-for i, antenna in enumerate(bodies[1:]):
-    antenna_shape = Cylinder(name='antenna{}'.format(i),
-                             radius=0.025,
-                             length=La1,
-                             color=farben[i])
+# for i, antenna in enumerate(bodies[1:]):
+#    antenna_shape = Cylinder(name='antenna{}'.format(i),
+#                             radius=0.025,
+#                             length=sys.constants[La],
+#                             color=farben[i])
 
-    viz_frames.append(VisualizationFrame('antenna_frame{}'.format(i),
-                                         frames[i+1],
-                                         mass_centers[i+1],
-                                         antenna,
-                                         antenna_shape))
+#    viz_frames.append(VisualizationFrame('antenna_frame{}'.format(i),
+#                                         frames[i+1],
+#                                         mass_centers[i+1],
+#                                         antenna,
+#                                         antenna_shape))
 
-explorer_shape = Cylinder(name='explorer',
-                          radius=reo1,
-                          length=Le1,
-                          color=farben[0])
+# explorer_shape = Cylinder(name='explorer',
+#                          radius=sys.constants[reo],
+#                          length=sys.constants[Le],
+#                          color=farben[0])
 
-viz_frames.append(VisualizationFrame('explorer_frame',
-                                     Be,
-                                     mass_centers[0],
-                                     explorer1,
-                                     explorer_shape))
+# viz_frames.append(VisualizationFrame('explorer_frame',
+#                                     Be,
+#                                     mass_centers[0],
+#                                     explorer1,
+#                                     explorer_shape))
 
-P_red_shape = Sphere(name='red_sphere',
-                     radius=0.05,
-                     color='red')
-viz_frames.append(VisualizationFrame('red_sphere_frame',
-                                     Be,
-                                     P_red,
-                                     P_red,
-                                     P_red_shape))
+# P_red_shape = Sphere(name='red_sphere',
+#                     radius=0.05,
+#                     color='red')
+# viz_frames.append(VisualizationFrame('red_sphere_frame',
+#                                     Be,
+#                                     P_red,
+#                                     P_red,
+#                                     P_red_shape))
 
-scene = Scene(N, O, *viz_frames)
+# scene = Scene(N, O, *viz_frames)
 
 # Provide the data to compute the trajectories of the visualization frames.
-scene.times = times
-scene.constants = dict(zip(pL, pL_vals))
-scene.states_symbols = q_ind + u_ind
-scene.states_trajectories = resultat
+# scene.times = sys. times
+# scene.constants = dict(zip(pL, pL_vals))
+# scene.states_symbols = q_ind + u_ind
+# scene.states_trajectories = resultat
 
 # scene.display_jupyter(axes_arrow_length=20)
 
@@ -593,22 +635,23 @@ scene.states_trajectories = resultat
 # Animation with Matplotlib
 # -------------------------
 
-fps = 10
+fps = 1
 
 # Create the end points of the antennas
 P01, P11, P21, P31, omega = sm.symbols('P01 P11 P21 P31 omega', cls=me.Point)
-P01.set_pos(P0, La * Aa0.y)
-P11.set_pos(P1, -La * Aa1.y)
-P21.set_pos(P2, La * Aa2.x)
-P31.set_pos(P3, -La * Aa3.x)
+P01.set_pos(P0, sys.constants[La] * Aa0.y)
+P11.set_pos(P1, -sys.constants[La] * Aa1.y)
+P21.set_pos(P2, sys.constants[La] * Aa2.x)
+P31.set_pos(P3, -sys.constants[La] * Aa3.x)
 omega.set_pos(Aoe, uex*Ae.x + uey*Ae.y + uez*Ae.z)
 
 coordinates = Aoe.pos_from(O).to_matrix(N)
-for point in (P0, P01, P1, P11, P2, P21, P3, P31, omega):
+for point in (P0, P01, P1, P11, P2, P21, P3, P31):
     coordinates = coordinates.row_join(point.pos_from(O).to_matrix(N))
+coordinates = coordinates.row_join(omega.pos_from(Aoe).to_matrix(N))
 coordinates_lam = sm.lambdify(qL + pL, coordinates, cse=True)
 
-t_arr = np.linspace(0, interval, schritte)
+t_arr = sys.times
 state_sol = CubicSpline(t_arr, resultat)
 
 # cylinder generation
@@ -676,9 +719,9 @@ def transform_cylinder(X, Y, Z, Aoe, qx, qy, qz):
 
 
 def init():
-    Le = Le1      # length
-    reo = reo1     # radius
-    X, Y, Z = make_cylinder_mesh(Le, reo, n_theta=16, n_z=8)
+    Le1 = sys.constants[Le]      # length
+    reo1 = sys.constants[reo]     # radius
+    X, Y, Z = make_cylinder_mesh(Le1, reo1, n_theta=16, n_z=8)
 
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(111, projection='3d')
@@ -706,9 +749,10 @@ def init():
     antenna2, = ax.plot([], [], [], color='black', lw=2)
     antenna3, = ax.plot([], [], [], color='black', lw=2)
     pfeil1 = ax.quiver(coords[0, 0], coords[1, 0], coords[2, 0],
-                       (coords[0, 9] - coords[0, 0]),
-                       (coords[1, 9] - coords[1, 0]),
-                       (coords[2, 9] - coords[2, 0]), color='red',
+                       coords[0, 9],
+                       coords[1, 9],
+                       coords[2, 9],
+                       color='red',
                        arrow_length_ratio=0.3,)
 
     return (fig, ax, surf, antenna0, antenna1, antenna2, antenna3, pfeil1,
@@ -724,8 +768,8 @@ def update(t):
     surf.remove()
     pfeil1.remove()
     ax.set_title(f"Running time: {t:.2f} sec \n The red arrow shows the "
-                 f"direction of the angular velocity \n "
-                 f"vector of the explorer", fontsize=15)
+                 f"direction and magnitude of the angular velocity \n "
+                 f"vector of the explorer", fontsize=12)
     coords = coordinates_lam(*state_sol(t), *pL_vals)
     # create explorer
     Aoe = np.array([coords[0, 0], coords[1, 0], coords[2, 0]])
@@ -745,23 +789,19 @@ def update(t):
     antenna3.set_data([coords[0, 7], coords[0, 8]],
                       [coords[1, 7], coords[1, 8]])
     antenna3.set_3d_properties([coords[2, 7], coords[2, 8]])
-    # As the angular velocity change substantially over time, it is scaled
-    # to be visible in the plot at all times.
-    scale_factor = 2.0 / np.sqrt((coords[0, 9] - coords[0, 0])**2 +
-                                 (coords[1, 9] - coords[1, 0])**2 +
-                                 (coords[2, 9] - coords[2, 0])**2)
+    scale_factor = 0.025
     pfeil1 = ax.quiver(coords[0, 0], coords[1, 0], coords[2, 0],
-                       scale_factor * (coords[0, 9] - coords[0, 0]),
-                       scale_factor * (coords[1, 9] - coords[1, 0]),
-                       scale_factor * (coords[2, 9] - coords[2, 0]),
+                       scale_factor * coords[0, 9],
+                       scale_factor * coords[1, 9],
+                       scale_factor * coords[2, 9],
                        color='red', arrow_length_ratio=0.2)
 
     return surf, antenna0, antenna1, antenna2, antenna3, pfeil1
 
 
-frames = np.concatenate((np.arange(0.0, 0.05 * interval, 1 / fps),
-                         np.arange(0.9 * interval, 0.95 * interval, 1 / fps)))
-ani = animation.FuncAnimation(fig, update, frames=frames, interval=1000 / fps,
+frames = np.concatenate((sys.times[: 25], sys.times[1000: 1050],
+                         sys.times[1975:]))
+ani = animation.FuncAnimation(fig, update, frames=frames, interval=50,
                               blit=False)
 
 plt.show()
